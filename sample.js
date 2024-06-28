616e1c2582ff0e03716150456f054669aec5f9f7
saurabhack <kamanesaurabh68@gmail.com>
2024-06-28 18:36:14 +0530

    first commit

---
+++ sample.js
@@ -0,0 +1,14 @@
+// let number=123
+// let reverse=0;
+// while(number!=0){
+//     let re=number%10;
+//     reverse=reverse*10+re;
+//     number=parseInt(number/10)
+// }
+// console.log(reverse);
+let number=12
+let num=12
+console.log(number!==num)
+let dd=1
+let dd2=2
+console.log(dd===dd2)
